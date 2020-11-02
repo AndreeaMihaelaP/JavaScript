0380e5c382bdbc8f5652d11147d5b2d8668fd8ca
Andreea.Patrascu <andreea.patrascu@SVROLP00107.local>
2020-11-02 17:44:18 +0200

    Added loader + tweet integration

--- quote-generator/script.js
+++ quote-generator/script.js
@@ -7,6 +7,20 @@ const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+// Show loading
+function loading() {
+	loader.hidden = false;
+	quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete() {
+	if(!loader.hidden) {
+		quoteContainer.hidden = false;
+		loader.hidden = true;
+	}
+}
+
 // Show new quote
 function newQuote() {
 	// Pick a random quote from apiQuotes array
@@ -16,11 +30,23 @@ function newQuote() {
 	} else {
 		authorText.innerText = quote.author;
 	}
-	console.log(quote);
+
+	// Reduce font size for long quotes
+	if(quote.text.length > 120) {
+		quoteText.classList.add('long-quote')
+	} else {
+		quoteText.classList.remove('long-quote');
+	}
+	
+	quoteText.innerText = quote.text;
+	
+	// Stop loader and show the quote
+	complete();
 }
 
 // Get Quotes From Api
 async function getQuotes() {
+	loading();
 	const apiUrl = 'https://type.fit/api/quotes';
 	try {
 		const response = await fetch(apiUrl);
@@ -31,6 +57,18 @@ async function getQuotes() {
 		console.log('whoops, no quote', error);
 	}
 }
+
+// Tweet Quote
+function tweetQuote() {
+	const quote = quoteText.innerText;
+	const author = authorText.innerText;
+	const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+	window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', getQuotes);
+twitterBtn.addEventListener('click', tweetQuote);
 
 // On Load
 getQuotes()
